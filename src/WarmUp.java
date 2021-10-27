e5764a4a634a398c3a1b11ddfb82a600663ff261
ivan <ivan.mgarcia125@gmail.com>
2021-10-27 13:21:48 +0200

    frontBack

--- src/WarmUp.java
+++ src/WarmUp.java
@@ -86,6 +86,11 @@ public class WarmUp {
             return str;
         return "Not " + str;
     }
+    public static String frontBack(String str) {
+        if (str.length() < 2)
+            return str;
+        return str.charAt(str.length()-1) + str.substring(1,str.length()-1) + str.charAt(0);
+    }
 
 
 }
